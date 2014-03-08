ab47a9840e5f9d3e86eedf1fe7c454c28af795c9
rajan patel <rp@gridvise.org>
2014-03-08 14:34:58 +0000

    Test dependency

---
+++ src/test/java/net/sparktheweb/tests/TestSparkWebPage.java
@@ -0,0 +1,18 @@
+package net.sparktheweb.tests;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.junit.Test;
+
+/**
+ * Created by rajan on 08/03/14.
+ */
+public class TestSparkWebPage {
+
+    @Test
+    public void testCount() {
+
+    }
+}
